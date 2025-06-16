fc3f5031395254f44077fff9fa4690f8d8e0c166
kexuan <kexuancai1223@gmail.com>
2025-06-16 23:36:19 +0800

    first commit mbm

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,32 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
-
+import tailwindcss from '@tailwindcss/vite';
+import sanity from '@sanity/astro';
+import react from '@astrojs/react';
+import staticAdapter from '@astrojs/adapter-static';
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+
+  site: 'https://kexuancai.github.io',
+  base: '/measure-by-measure',
+
+
+  adapter: staticAdapter(),
+  vite: {
+    plugins: [tailwindcss()],
+    optimizeDeps: {
+      exclude: ['../../sanity.config.ts'],
+    },
+  },
+
+  integrations: [
+    sanity({
+      projectId: 'n13ym9gs',
+      dataset: 'production',
+      // Set useCdn to false if you're building statically.
+      useCdn: false,
+      studioBasePath: '/admin',
+
+    }), 
+    react()
+  ]
+});
