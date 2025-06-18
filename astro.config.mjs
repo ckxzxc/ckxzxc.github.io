2f6b9adac39d1177684ff621d0b5e611b0265702
kexuan <kexuancai1223@gmail.com>
2025-06-19 03:50:39 +0800

    remove cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,32 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
-import sanity from '@sanity/astro';
-import react from '@astrojs/react';
-import staticAdapter from '@astrojs/adapter-static';
+
 // https://astro.build/config
 export default defineConfig({
 
   site: 'https://ckxzxc.github.io',
   base: '/measure-by-measure',
 
-
-  adapter: staticAdapter(),
   vite: {
     plugins: [tailwindcss()],
     optimizeDeps: {
-      exclude: ['../../sanity.config.ts'],
     },
   },
-
-  integrations: [
-    sanity({
-      projectId: 'n13ym9gs',
-      dataset: 'production',
-      // Set useCdn to false if you're building statically.
-      useCdn: false,
-      studioBasePath: '/admin',
-
-    }), 
-    react()
-  ]
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -1,4 +1,42 @@
 import { defineCollection, z } from 'astro:content';
+import { file, glob } from 'astro/loaders';
+import { string } from 'astro:schema';
+
+
+const categories = defineCollection({
+  type: "data",
+  schema: z.object({
+    title: z.string(),
+    slug: z.string(),
+    description: z.string()
+  }),
+});
+
+const exerciseCollections = defineCollection({
+  type: "data",
+  schema:z.object({
+    title: z.string(),
+    slug: z.string(),
+    category: z.string(),
+    level: z.string(),
+    description: z.string()
+  }),
+});
+
+const exercises = defineCollection({
+  type: "data",
+  schema:z.object({
+    index: z.number(),
+    title: z.string(),
+    slug: z.string(),
+    category: z.string(),
+    collection: z.string(),
+    templateABC: z.string(),
+    hintABC: z.string(),
+    solutionABC: z.string()
+  }),
+
+});
 
 const exerciseCollectionDocs = defineCollection({
   schema: z.object({
@@ -8,5 +46,9 @@ const exerciseCollectionDocs = defineCollection({
 });
 
 export const collections = {
+  categories,
+  exerciseCollections,
+  exercises,
   exerciseCollectionDocs,
+
 };
